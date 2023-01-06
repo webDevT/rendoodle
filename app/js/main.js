c8b3d5929c2a9ede1f3bb7a8adaaeb9cd0bd66b7
Karina <karinadesyatnik27@gmail.com>
2023-01-06 14:31:51 +0300

    new-pages

--- app/js/main.js
+++ app/js/main.js
@@ -44,3 +44,13 @@ $('.slider').slick({
     ]
   });
 //end slider init
+
+//start floorplan tabs
+$(function(){
+	$(".tab").click(function() {
+	$(".tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab-item").removeClass("active").eq($(this).index()).addClass("active");
+ 	$(".tab-item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+});
+//end floorplan tabs
